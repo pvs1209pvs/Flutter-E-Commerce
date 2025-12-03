1a0727347d482b71653a789dc7ea9f01c3cb9088
Paramvir <pvs.1209@gmail.com>
2025-12-03 10:23:25 +0530

    Cart total provider

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -1,9 +1,12 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:developer';
+import 'dart:nativewrappers/_internal/vm/lib/ffi_native_type_patch.dart';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter_e_commerce/models/product.dart';
+import 'package:flutter_e_commerce/models/product_cart.dart';
+import 'package:flutter_e_commerce/models/product_with_quantity.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 
@@ -53,28 +56,91 @@ final productProvider = AsyncNotifierProvider<ProductNotifier, List<Product>>(
 );
 
 final filterProd = Provider<AsyncValue<List<Product>>>((ref) {
-  final searchRef = ref.watch(searchQueryNotifierProvider);
+
+  
+  final searchQuery = ref.watch(searchQueryNotifierProvider);
   final categoryQuery = ref.watch(categoryQueryNotifierProvider);
-  log("search query $searchRef");
+
+  log("search query $searchQuery");
   log("category query $categoryQuery");
+
   final filteredProducts = ref.watch(productProvider).whenData((cb) {
-    if ((searchRef == null || searchRef.isEmpty) &&
+    if ((searchQuery == null || searchQuery.isEmpty) &&
         (categoryQuery == null || categoryQuery == "All")) {
       log("returning original");
       return cb;
-    } else {
-      return cb
-          .where(
-            (test) =>
-                test.title.toLowerCase().contains(
-                  searchRef.toString().toLowerCase(),
-                ) ||
-                test.category.toLowerCase().contains(
-                  categoryQuery.toString().toLowerCase(),
-                ),
-          )
-          .toList();
     }
+
+    return cb
+        .where(
+          (test) =>
+              test.title.toLowerCase().contains(
+                searchQuery.toString().toLowerCase(),
+              ) ||
+              test.category.toLowerCase().contains(
+                categoryQuery.toString().toLowerCase(),
+              ),
+        )
+        .toList();
   });
+
   return filteredProducts;
+});
+
+class ShoppingCartNotifier extends AsyncNotifier<List<ProductWithQuantity>> {
+  @override
+  FutureOr<List<ProductWithQuantity>> build() async {
+    // get id of product in cart
+    http.Response response = await http.get(
+      Uri.parse("https://fakestoreapi.com/carts/1"),
+    );
+
+    // products from id
+    var productsInCart = ProductCart.fromJson(
+      jsonDecode(response.body),
+    ).products;
+
+    var products = productsInCart
+        .map(
+          (productCartSaved) => http.get(
+            Uri.parse(
+              "https://fakestoreapi.com/products/${productCartSaved.productId}",
+            ),
+          ),
+        )
+        .toList();
+
+    var temp = (await Future.wait(
+      products,
+    )).map((response) => Product.fromJson(jsonDecode(response.body))).toList();
+
+    List<ProductWithQuantity> prodsWithQuantity = [];
+
+    for (var i = 0; i < products.length; i++) {
+      prodsWithQuantity.add(
+        ProductWithQuantity(
+          product: temp[i],
+          quantity: productsInCart[i].quantity!,
+        ),
+      );
+    }
+
+    return prodsWithQuantity;
+  }
+}
+
+final shoppingCartNotifierProvider =
+    AsyncNotifierProvider<ShoppingCartNotifier, List<ProductWithQuantity>>(
+      () => ShoppingCartNotifier(),
+    );
+
+final cartTotalProvider = Provider<double>((ref) {
+  final cartProducts = ref.watch(shoppingCartNotifierProvider);
+
+  return cartProducts.maybeWhen(
+    data: (state) => state
+        .map((prod) => prod.product.price * prod.quantity)
+        .fold(0.0, (prev, value) => prev + value),
+    orElse: () => 0.0,
+  );
 });
